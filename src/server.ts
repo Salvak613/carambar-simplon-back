ace93defd5d5a200e952838d92da15d8e5fe2911
Jules Clauwaert <jules.clauwaert@gmail.com>
2025-09-02 01:27:12 +0200

    fix for deployment

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import app from "./app";
 
-const PORT = Number(process.env.PORT);
+const PORT = Number(process.env.PORT) || 3000;
 
 app.listen(PORT, () => {
   console.log(`🚀 Serveur lancé sur http://localhost:${PORT}`);
